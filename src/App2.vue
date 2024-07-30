f9ab3577335181986b9fb276a092947ad3cb43b9
Joji Shiotsuki <149657863+jojiShiotsuki@users.noreply.github.com>
2024-07-30 16:18:43 +0800

    first commit

---
+++ src/App2.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { ref, onMounted} from 'vue';
+
+    const name = ref('John Doe');
+    const status = ref('active');
+    const tasks = ref(['task one', 'task two', 'task three']);
+    const newTask = ref('')
+
+    const toggleStatus = () => {
+      if(status.value === 'active') {
+        status.value = 'pending';
+      } else if(status.value === 'pending'){
+        status.value = 'inactive'
+      }else{
+        status.value = 'active'
+      }
+    }
+
+    const addTask = () => {
+      if(newTask.value.trim() !== ''){
+        tasks.value.push(newTask.value)
+        newTask.value = '';
+      }
+    }
+
+    const deleteTask = (index) => {
+      tasks.value.splice(index, 1)
+    }
+
+onMounted(async () => {
+  try {
+    const response = await fetch ('https://jsonplaceholder.typicode.com/todos');
+    const data = await response.json();
+    tasks.value = data.map((task) => task.title)
+  } catch(error) {
+    console.log('Error Fetching tasks')
+  }
+});
+</script>
+
+
+<template>
+  <h1>{{ name }}</h1>
+  <br>
+  <p v-if="status === 'active'"> User is active</p>
+  <p v-else-if="status === 'pending'"> User is pending</p>
+  <p v-else> User is inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask">
+    <button type="submit">submit</button>
+  </form>
+
+  <br>
+  <h3>Tasks: </h3>
+  <ul>
+    <li v-for="(task, index) in tasks" :key="task"> 
+      <span>
+        {{ task }}
+      </span>
+      <button @click="deleteTask(index)">X</button>
+    </li>
+  </ul>
+  <br>
+  <button @click="toggleStatus">Change status</button>
+</template>
