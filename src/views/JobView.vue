9ee8086a05fca85b07e7ed74b53730afc97cce40
Joji Shiotsuki <149657863+jojiShiotsuki@users.noreply.github.com>
2024-08-01 14:44:57 +0800

    added update, delete function for jobs, also added add job page

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -2,10 +2,14 @@
 import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 import axios from 'axios';
 import { reactive, onMounted } from 'vue';
-import { useRoute, RouterLink } from 'vue-router';
+import { useRoute, RouterLink, useRouter } from 'vue-router';
 import BackButton from '@/components/BackButton.vue';
+import { useToast } from 'vue-toastification';
+
 
 const route = useRoute();
+const router = useRouter();
+const toast = useToast();
 
 const jobId = route.params.id;
 
@@ -14,9 +18,23 @@ const state = reactive({
   isLoading: true,
 });
 
+const deleteJob = async() => {
+  try {
+    const confirm = window.confirm('Are you sure you want to delete this job?')
+    if(confirm){
+      await axios.delete(`/api/jobs/${jobId}`);
+      toast.success('Job Deleted Successfully')
+      router.push('/jobs');
+    }
+  } catch (error) {
+    console.error('Error deleting job', error);
+    toast.error('Job Not Deleted');
+  }
+}
+
 onMounted(async () => {
 	try{
-		const response = await axios.get(`http://localhost:5000/jobs/${jobId}`)
+		const response = await axios.get(`/api/jobs/${jobId}`)
 		state.job = response.data;
 	} catch(error) {
 		console.error('Error fetching job', error)
@@ -42,7 +60,7 @@ onMounted(async () => {
                 class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
               >
                 <i
-                  class="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
+                  class="pi pi-map-marker text-xl text-orange-700 mr-2"
                 ></i>
                 <p class="text-orange-700">{{ state.job.location }}</p>
               </div>
@@ -96,7 +114,7 @@ onMounted(async () => {
                 class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job
               </RouterLink>
-              <button
+              <button @click="deleteJob"
                 class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
